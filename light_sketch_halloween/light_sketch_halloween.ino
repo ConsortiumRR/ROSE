409fd7fd78b192288bca163686f5927955781960
OferShouval <45494179+OferShouval@users.noreply.github.com>
2020-10-30 16:41:25 -0400

    Update light_sketch_halloween.ino

--- light_sketch_halloween/light_sketch_halloween.ino
+++ light_sketch_halloween/light_sketch_halloween.ino
@@ -7,32 +7,56 @@ unsigned long pulseDuration;
 
 void setup() 
 {
+  Serial.begin(115200);
   CircuitPlayground.begin();
+  
 
-  pinMode(colorPulse, INPUT);
-  pinMode(READPIN2, INPUT);
+  pinMode(colorPulsePin, INPUT);
+  pinMode(onOffPin, INPUT);
   
 }
 
 void loop()
 {
-  pulseDuration = pulseIn(READPIN2, HIGH);
   
-  if (digitalRead(onOffPin)==LOW){
-     CircuitPlayground.fill(0);
-  }
-  else
+  //pulseDuration = pulseIn(colorPulsePin, HIGH);
+   //Serial.println(pulseDuration);
+  
+  
+  if (digitalRead(onOffPin)==LOW)
   {
-     if (1500 > pulseDuration > 2500) //Orange -> 2 millisecond pulse from bot
-     {
-      CircuitPlayground.fill(255,140,0);
+     for(int x = 0; x<10;x++){
+      CircuitPlayground.setPixelColor(x,0,0,0);
      }
-     if (2500 > pulseDuration > 3500) //yellow -> 3 millisecond pulse from bot
-     {
-      CircuitPlayground.fill(244,232,104);
-     }
+     Serial.println("low");
   }
   
-strip.show();
+  if (digitalRead(onOffPin)==HIGH){
+  
+  for(int y = 0; y<10; y++)
+      { 
+        CircuitPlayground.setPixelColor(y,255,140,0);
+      }
+   Serial.println("high");
+    
+//     if (4500 > pulseDuration > 5500) //Orange -> 5 millisecond pulse from bot
+//     {
+//      for(int y = 0; y<10; y++)
+//      { 
+//        CircuitPlayground.setPixelColor(y,255,140,0);
+//      }
+//      
+//     }
+//     
+//     if (9500 > pulseDuration > 10500) //yellow -> 10 millisecond pulse from bot
+//     {
+//      for(int z = 0; z<10 ; z++)
+//      { 
+//        CircuitPlayground.setPixelColor(z,244,232,104);
+//      }
+//      
+//     }
+  }
+  
 
 }
